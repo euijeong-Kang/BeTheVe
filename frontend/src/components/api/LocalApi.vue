643fd6ba6c7cdbadcf16885ce842c802ddc6579a
euijeong-Kang <dmlwjd558@gmail.com>
2022-02-15 20:37:58 +0900

    branch test

---
+++ frontend/src/components/api/LocalApi.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <h1>Daum Postcode API with Vuejs</h1>
+    <div class="postcode-input-wrapper">
+      <b-form-group
+        id="fieldset1"
+        description="Enter your Address"
+        label="Address1"
+      >
+        <b-form-input
+          class="address1"
+          id="input1"
+          readonly="readonly"
+          v-model="userFullAddress"
+        ></b-form-input>
+        <b-button @click="showModal" variant="success">Find Address</b-button>
+      </b-form-group>
+      <b-form-group
+        id="fieldset2"
+        description="Enter your detail Address"
+        label="Address2"
+      >
+        <b-form-input id="input2" v-model="userDetailAddress"></b-form-input>
+      </b-form-group>
+    </div>
+
+    <!-- Daum API Modal -->
+    <DaumPostCodeModal
+      @setUserData="setUserData"
+      ref="modal"
+    ></DaumPostCodeModal>
+    <!--// Daum API Modal -->
+  </div>
+</template>
+
+<script>
+import DaumPostCodeModal from './Modal.vue';
+export default {
+  data() {
+    return {
+      userFullAddress: null,
+      userDetailAddress: null,
+    };
+  },
+  methods: {
+    showModal() {
+      this.$refs.modal.show();
+    },
+    setUserData(payload) {
+      this.userFullAddress = payload.fullAddress;
+    },
+  },
+  components: {
+    DaumPostCodeModal,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.postcode-input-wrapper {
+  width: 600px;
+  margin: 0 auto;
+  text-align: left;
+}
+.form-control {
+  display: inline-block;
+  vertical-align: top;
+}
+.address1 {
+  width: 475px;
+}
+</style>
