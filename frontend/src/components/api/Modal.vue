8d58db400138c07b6338d6739d6cb7ccde8833d4
euijeong-Kang <dmlwjd558@gmail.com>
2022-02-16 10:49:45 +0900

    test commit

---
+++ frontend/src/components/api/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-modal id="myModal" title="Find your address!" ref="modal" hide-footer>
+    <div class="modal-contents" id="contents"></div>
+  </b-modal>
+</template>
+<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+<script>
+export default {
+  name: 'DaumPostCodeModal',
+  methods: {
+    show() {
+      this.setAPIContents();
+      this.$refs.modal.show();
+    },
+    setAPIContents() {
+      const targetElement = document.getElementById('contents');
+      const vm = this;
+      new daum.Postcode({
+        oncomplete: function (data) {
+          let fullAddr = data.address;
+          let extraAddr = '';
+          if (data.addressType === 'R') {
+            if (data.bname !== '') {
+              extraAddr += data.bname;
+            }
+            if (data.buildingName !== '') {
+              extraAddr +=
+                extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+            }
+            fullAddr += extraAddr !== '' ? ' (' + extraAddr + ')' : '';
+          }
+          vm.$emit('setUserData', {
+            postCode: data.zonecode,
+            fullAddress: fullAddr,
+            englishFullAddress: data.addressEnglish,
+          });
+          vm.$refs.modal.hide();
+        },
+        width: '100%',
+        height: '100%',
+        maxSuggestItems: 5,
+      }).embed(targetElement);
+    },
+  },
+};
+</script>
+
+<style>
+.modal-contents {
+  width: 100%;
+  height: 450px;
+}
+</style>
